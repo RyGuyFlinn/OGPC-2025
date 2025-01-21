4b7aae9c65da0581d31c6988e1a252e96c2152a4
RyGuyFlinn <ryguyflinn@gmail.com>
2025-01-21 11:45:16 -0800

    Gun Flips Vertically When Mouse x is less

--- The Beauty Planet/Assets/Scripts/GunController.cs
+++ The Beauty Planet/Assets/Scripts/GunController.cs
@@ -6,6 +6,7 @@ public class GunController : MonoBehaviour
 {
     public GameObject projectile;
     public GameObject player;
+    public SpriteRenderer sprite;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +16,15 @@ public class GunController : MonoBehaviour
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Input.mousePosition.x < Camera.main.WorldToScreenPoint(transform.position).x)
+        {
+            sprite.flipY = true;
+        }
+        else
+        {
+            sprite.flipY = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
