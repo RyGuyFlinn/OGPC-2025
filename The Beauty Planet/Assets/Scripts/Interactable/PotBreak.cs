8074e54631c70ca2be424ab781169143ceb3df34
Hall1day <199250@student.vansd.org>
2025-03-05 17:00:55 -0800

    Added Pot Breaking
    Pot Breaking is still buggy with the bullets.

--- The Beauty Planet/Assets/Scripts/Interactable/PotBreak.cs
+++ The Beauty Planet/Assets/Scripts/Interactable/PotBreak.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PotBreak : MonoBehaviour
 {
+    public GameObject break_1;
+    public GameObject break_2;
+    public GameObject break_3;
+    public GameObject break_4;
+    public GameObject pot;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,34 @@ public class PotBreak : MonoBehaviour
     {
         
     }
-}
+    void OnTriggerStay2D(Collider2D Collide){
+    if (Collide.tag == "Player"){
+        if (Input.GetKey(KeyCode.E)){
+            break_1.SetActive(true);
+            break_2.SetActive(true);
+            break_3.SetActive(true);
+            break_4.SetActive(true);
+            
+            pot.GetComponent<BoxCollider2D>().enabled = false;
+            pot.GetComponent<SpriteRenderer>().enabled = false;
+            StartCoroutine(Wait());
+        } }
+    if (Collide.tag == "Bullet"){
+        print("Bullet Collide!");
+        Destroy(Collide);
+            break_1.SetActive(true);
+            break_2.SetActive(true);
+            break_3.SetActive(true);
+            break_4.SetActive(true);
+            
+            pot.GetComponent<BoxCollider2D>().enabled = false;
+            pot.GetComponent<SpriteRenderer>().enabled = false;
+            StartCoroutine(Wait());
+    }
+    }
+     
+     IEnumerator Wait(){
+        yield return new WaitForSeconds(1);
+        Destroy(pot);
+     } }
+
