8df1dc8c2aafbcef796c4422090d2bd691df18b7
RyGuyFlinn <ryguyflinn@gmail.com>
2025-03-24 19:01:42 -0700

    Fixed the screen shake

--- The Beauty Planet/Assets/Scripts/Player/Shake.cs
+++ The Beauty Planet/Assets/Scripts/Player/Shake.cs
@@ -7,7 +7,13 @@ public class Shake : MonoBehaviour
     public bool start = false;
     public AnimationCurve curve;
     public float duration = 1f;
+
+    private GameObject player;
     
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
     void Update()
     {
         if (start)
@@ -19,17 +25,18 @@ public class Shake : MonoBehaviour
 
     IEnumerator Shaking()
     {
-        Vector3 startPosition = transform.position;
+        Vector3 startPosition = new Vector3(player.transform.position.x, player.transform.position.y, -50);
         float elapsedTime = 0f;
 
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
             float strength = curve.Evaluate(elapsedTime / duration);
-            transform.position = startPosition + Random.insideUnitSphere * strength;
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -50) + Random.insideUnitSphere * strength;
+
             yield return null;
         }
 
-        transform.position = startPosition;
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -50);
     }
 }
