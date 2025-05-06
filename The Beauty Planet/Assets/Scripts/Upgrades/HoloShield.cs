bfcd47913ec49e886f295caa6ec9030d45699afa
RyGuyFlinn <ryguyflinn@gmail.com>
2025-05-06 10:43:30 -0700

    Added blaster art

--- The Beauty Planet/Assets/Scripts/Upgrades/HoloShield.cs
+++ The Beauty Planet/Assets/Scripts/Upgrades/HoloShield.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class HoloShield : MonoBehaviour
 {
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        transform.position = player.gameObject.transform.position;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("collided with" + other + ", " + other.tag);
